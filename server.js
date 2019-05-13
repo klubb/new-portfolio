4eaad6cbea650b73e99dbb60502ae4e199829b42
klubb <unchartedanthem@gmail.com>
2019-05-13 17:58:23 -0600

    changes

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const express = require('express'),
 app = express(),
 { SERVER_PORT } = process.env 
 
-app.use(express.static(`${__dirname}`))
+app.use(express.static( `${__dirname}/../build` ))
 
 app.listen(SERVER_PORT, () => console.log(`Listening on PORT: ${SERVER_PORT} 🔥`))
